804f452eadc226abfb7c53be56df445781b46ca2
tuan17tuii <136962397+tuan17tuii@users.noreply.github.com>
2023-07-12 15:26:38 +0700

    add

--- PJ/storage/framework/views/578f4f73b882e6cddd0e71ced2b5cbce.php
+++ PJ/storage/framework/views/578f4f73b882e6cddd0e71ced2b5cbce.php
@@ -13,15 +13,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <!-- Css Styles -->
-    <link rel="stylesheet" href="{{ asset('user') }}/css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="{{ asset('user') }}/css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" href="{{ asset('user') }}/css/elegant-icons.css" type="text/css">
-    <link rel="stylesheet" href="{{ asset('user') }}/css/magnific-popup.css" type="text/css">
-    <link rel="stylesheet" href="{{ asset('user') }}/css/nice-select.css" type="text/css">
-    <link rel="stylesheet" href="{{ asset('user') }}/css/owl.carousel.min.css" type="text/css">
-    <link rel="stylesheet" href="{{ asset('user') }}/css/slicknav.min.css" type="text/css">
-    <link rel="stylesheet" href="{{ asset('user') }}/css/style.css" type="text/css">
-    <link rel="stylesheet" href="{{ asset('user') }}/css/style__.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo e(asset('user')); ?>/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo e(asset('user')); ?>/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo e(asset('user')); ?>/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo e(asset('user')); ?>/css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo e(asset('user')); ?>/css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo e(asset('user')); ?>/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo e(asset('user')); ?>/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo e(asset('user')); ?>/css/style.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo e(asset('user')); ?>/css/style__.css" type="text/css">
 
 </head>
 
@@ -33,13 +33,13 @@
                 <div class="row">
                     <div class="col-md-3">
                         <div class="mylogo">
-                            <a href="{{ url('/') }}"><img src="{{ asset('user') }}/img/logo.png" alt=""></a>
+                            <a href="<?php echo e(url('/')); ?>"><img src="<?php echo e(asset('user')); ?>/img/logo.png" alt=""></a>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="right_header">
                             <div class="shop__sidebar__search">
-                                <form action="{{ url('/search') }}">
+                                <form action="<?php echo e(url('/search')); ?>">
                                     <input type="text" placeholder="Search..." name="search">
                                     <button type="submit"><span class="icon_search"></span></button>
                                 </form>
@@ -50,42 +50,43 @@
                         <nav class="icon__menu mobile-menu">
                             <ul style="margin:30px  0px 0px 60px">
                                 <li>
-                                    @if (session()->has('username'))
-                                    <a style="color:black;display:block" href="{{ url('/account') }}"><i style="font-size: 25px;" class="fa fa-user-circle-o" aria-hidden="true"></i>
-                                    </a> {{ session('username') }}
-                                    <ul class="dropdown">
-                                        <li><a href="{{ url('/account') }}">Information</a></li>
+                                    <?php if(session()->has('username')): ?>
+                                    <a style="color:black;display:block" href="<?php echo e(url('/account')); ?>"><i style="font-size: 25px;" class="fa fa-user-circle-o" aria-hidden="true"></i>
+                                    </a> <?php echo e(session('username')); ?>
 
-                                        <li><a href="{{ url('/login/logout') }}">Logout</a></li>
+                                    <ul class="dropdown">
+                                        <li><a href="<?php echo e(url('/account')); ?>">Information</a></li>
+
+                                        <li><a href="<?php echo e(url('/login/logout')); ?>">Logout</a></li>
                                     </ul>
-                                    @else
-                                    <a style="color:black;display:block" href="{{ url('login/signin') }}"><i style="font-size: 25px;" class="fa fa-user-circle-o" aria-hidden="true"></i>
+                                    <?php else: ?>
+                                    <a style="color:black;display:block" href="<?php echo e(url('login/signin')); ?>"><i style="font-size: 25px;" class="fa fa-user-circle-o" aria-hidden="true"></i>
                                     </a>Sign in
-                                    @endif
+                                    <?php endif; ?>
 
                                 </li>
                                 <li>
-                                    @if (session()->has('username'))
-                                        <a href="{{ url('pages/shoppingcart') }}" style="color:black"><i
+                                    <?php if(session()->has('username')): ?>
+                                        <a href="<?php echo e(url('pages/shoppingcart')); ?>" style="color:black"><i
                                                 style="font-size: 25px;" class="fa fa-shopping-basket"></i></a>
                                         <a style="color:black" href=""></a><br>Cart
                                         <ul class="dropdown">
                                             <li style="background-color: #ff0000;"><a
-                                                    href="{{ url('/pages/shoppingcart') }}">giỏ
+                                                    href="<?php echo e(url('/pages/shoppingcart')); ?>">giỏ
                                                     hàng tạm thời</a></li>
                                             <li style="background-color: #ffcc00;"><a
-                                                    href="{{ url('/shop/savedcart') }}">giỏ
+                                                    href="<?php echo e(url('/shop/savedcart')); ?>">giỏ
                                                     hàng saved</a></li>
                                             <li style="background-color: #00ff04;"><a
-                                                    href="{{ url('/shop/paidcart') }}">paid shopping cart</a></li>
+                                                    href="<?php echo e(url('/shop/paidcart')); ?>">paid shopping cart</a></li>
                                         </ul>
-                                    @else
-                                        <a href="{{ url('login/signin') }}" style="color:black"><i
+                                    <?php else: ?>
+                                        <a href="<?php echo e(url('login/signin')); ?>" style="color:black"><i
                                                 style="font-size: 25px;" class="fa fa-shopping-basket"></i></a>
                                         <a style="color:black" href=""></a><br>Cart
-                                    @endif
-                                    {{-- <a href="#"><img src="{{asset('user')}}/img/icon/heart.png" alt=""></a> --}}
-                                    {{-- <div class="price">$0.00</div> --}}
+                                    <?php endif; ?>
+                                    
+                                    
 
                                 </li>
                             </ul>
@@ -104,17 +105,17 @@
                 <div class="col-lg-8 col-md-8">
                     <nav class="header__menu mobile-menu">
                         <ul>
-                            <li><a href="{{ url('/') }}">Home</a></li>
-                            <li><a href="{{ url('/shop') }}">Shop</a></li>
+                            <li><a href="<?php echo e(url('/')); ?>">Home</a></li>
+                            <li><a href="<?php echo e(url('/shop')); ?>">Shop</a></li>
                             <li><a href="#">Pages</a>
                                 <ul class="dropdown">
-                                    <li><a href="{{ url('/pages/aboutus') }}">About Us</a></li>
-                                    <li><a href="{{ url('/pages/shoppingcart') }}">Shopping Cart</a></li>
+                                    <li><a href="<?php echo e(url('/pages/aboutus')); ?>">About Us</a></li>
+                                    <li><a href="<?php echo e(url('/pages/shoppingcart')); ?>">Shopping Cart</a></li>
 
                                 </ul>
                             </li>
-                            <li><a href="{{ url('/blog') }}">Blog</a></li>
-                            <li><a href="{{ url('/contacts') }}">Contacts</a></li>
+                            <li><a href="<?php echo e(url('/blog')); ?>">Blog</a></li>
+                            <li><a href="<?php echo e(url('/contacts')); ?>">Contacts</a></li>
                         </ul>
                     </nav>
                 </div>
@@ -131,7 +132,7 @@
     <!-- Header Section End -->
 
     <!-- Hero Section Begin -->
-    @yield('content')
+    <?php echo $__env->yieldContent('content'); ?>
     <!-- Latest Blog Section End -->
 
     <!-- Footer Section Begin -->
@@ -141,7 +142,7 @@
                 <div class="col-md-3">
                     <div class="footer__about">
                         <div class="footer__logo">
-                            <a href="#"><img src="{{ asset('user') }}/img/footer-logo.png" alt=""></a>
+                            <a href="#"><img src="<?php echo e(asset('user')); ?>/img/footer-logo.png" alt=""></a>
                         </div>
                         <p>The customer is at the heart of our unique business model, which includes design.</p>
 
@@ -208,17 +209,17 @@
     <!-- Search End -->
 
     <!-- Js Plugins -->
-    <script src="{{ asset('user') }}/js/jquery-3.3.1.min.js"></script>
-    <script src="{{ asset('user') }}/js/bootstrap.min.js"></script>
-    <script src="{{ asset('user') }}/js/jquery.nice-select.min.js"></script>
-    <script src="{{ asset('user') }}/js/jquery.nicescroll.min.js"></script>
-    <script src="{{ asset('user') }}/js/jquery.magnific-popup.min.js"></script>
-    <script src="{{ asset('user') }}/js/jquery.countdown.min.js"></script>
-    <script src="{{ asset('user') }}/js/jquery.slicknav.js"></script>
-    <script src="{{ asset('user') }}/js/mixitup.min.js"></script>
-    <script src="{{ asset('user') }}/js/owl.carousel.min.js"></script>
-    <script src="{{ asset('user') }}/js/main.js"></script>
-    <script src="{{ asset('user') }}/js/shopcart.js"></script>
+    <script src="<?php echo e(asset('user')); ?>/js/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo e(asset('user')); ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo e(asset('user')); ?>/js/jquery.nice-select.min.js"></script>
+    <script src="<?php echo e(asset('user')); ?>/js/jquery.nicescroll.min.js"></script>
+    <script src="<?php echo e(asset('user')); ?>/js/jquery.magnific-popup.min.js"></script>
+    <script src="<?php echo e(asset('user')); ?>/js/jquery.countdown.min.js"></script>
+    <script src="<?php echo e(asset('user')); ?>/js/jquery.slicknav.js"></script>
+    <script src="<?php echo e(asset('user')); ?>/js/mixitup.min.js"></script>
+    <script src="<?php echo e(asset('user')); ?>/js/owl.carousel.min.js"></script>
+    <script src="<?php echo e(asset('user')); ?>/js/main.js"></script>
+    <script src="<?php echo e(asset('user')); ?>/js/shopcart.js"></script>
     <div class="fix_tel">
         <div class="ring-alo-phone ring-alo-green ring-alo-show" id="ring-alo-phoneIcon" style="right: 150px; bottom: -12px;">
             <div class="ring-alo-ph-circle"></div>
@@ -243,4 +244,4 @@
 
 </body>
 
-</html>
+</html><?php /**PATH C:\Users\tn018\Desktop\ALO\Laravel\PJ\resources\views/user/layout/layout.blade.php ENDPATH**/ ?>
---
+++ PJ/storage/framework/views/a3954ddee572b5fa7710beea027082f8.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Admin</title>
+    <link rel="stylesheet" href="<?php echo e(asset('admin/css')); ?>/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    
+    <?php $__env->startSection('content'); ?>
+    <!-- Content Header (Page header) -->
+    <div align="center">
+        <h2>Welcome Admin</h2>
+    </div>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Info boxes -->
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="info-box mb-3">
+                        <span class="info-box-icon bg-success elevation-1"><a href="<?php echo e(url('/admin/productlist')); ?>" class="fas fa-boxes"></a></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">Number of Product</span>
+                            <span class="info-box-number"><?php echo e($number); ?></span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+                <div class="col-md-6">
+                    <div class="info-box mb-3">
+                        <span class="info-box-icon bg-warning elevation-1"><a href="#" class="fas fa-users"></a></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">Total Members</span>
+                            <span class="info-box-number"><?php echo e($users); ?></span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+
+            <!-- Main row -->
+            <div class="row">
+                <!-- Left col -->
+                <div class="col-md-8">
+                    <!-- TABLE: LATEST ORDERS -->
+                    <div class="card">
+                        <div class="card-header border-transparent">
+                            <h3 class="card-title">Latest Orders</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body p-0">
+                            <div class="table-responsive">
+                                <table class="table m-0">
+                                    <thead>
+                                        <tr>
+                                            <th>Customer</th>
+                                            <th>Email</th>
+                                            <th>Address</th>
+                                            <th>To Details</th>
+                                        </tr>
+                                    </thead>
+                                    <?php $__currentLoopData = $orders; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $order): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                        <?php if($order->status == 1): ?>
+                                            <tbody>
+                                                <input type="hidden" name="order_id" ">
+                                                <td><?php echo e($order->username); ?></td>
+                                                <td><?php echo e($order->email); ?></td>
+                                                <td><?php echo e($order->address); ?></td>
+                                                <td>
+                                                    <a href=" <?php echo e(url('/admin/order_details/'.$order->id)); ?>">Order's detail</a>
+                                                </td>
+                                            </tbody>
+                                        <?php endif; ?>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                </table>
+                            </div>
+                            <!-- /.table-responsive -->
+                        </div>
+                        <!-- /.card-body -->
+                        <div class=" card-footer clearfix">
+                            <a href="<?php echo e(url('/admin/orderslist')); ?>" class="btn btn-sm btn-secondary float-right">View All Orders</a>
+                        </div>
+                        <!-- /.card-footer -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+
+                <div class="col-md-4">
+                    <!-- PRODUCT LIST -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Recently Added Products</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body p-0">
+                            <ul class="products-list product-list-in-card pl-2 pr-2">
+                                <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <li class="item">
+                                    <div class="product-img">
+                                        <img src="<?php echo e(asset('user/img/product')); ?>/<?php echo e($product->photo); ?>" alt="Product Image" class="img-size-50">
+                                    </div>
+                                    <div class="product-info">
+                                        <?php echo e($product->name); ?>
+
+                                        <span class="badge badge-warning float-right"><?php echo e($product->price); ?>$</span></a>
+                                        <span class="product-description">
+                                            <?php echo e($product->details); ?>
+
+                                        </span>
+                                    </div>
+                                </li>
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                            </ul>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer text-center">
+                            <a href="<?php echo e(url('/admin/productlist')); ?>" class="uppercase">View All Products</a>
+                        </div>
+                        <!-- /.card-footer -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div><!--/. container-fluid -->
+    </section>
+    <!-- /.content -->
+    <?php $__env->stopSection(); ?>
+</body>
+
+</html>
+<?php echo $__env->make('admin.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\tn018\Desktop\ALO\Laravel\PJ\resources\views/Admin/MainPage/dashboard.blade.php ENDPATH**/ ?>
